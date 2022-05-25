3b11d71e27e9e2785aaf04ada690a1bdc2043474
Eunjung0404 <95silver44@gmail.com>
2022-05-25 16:13:41 +0900

    chart부분 수정중...

--- js_study/WebContent/WEB-INF/Chart/js/chart.js
+++ js_study/WebContent/WEB-INF/Chart/js/chart.js
@@ -1,25 +1,25 @@
 const chart1=document.querySelector('.chart1');
 const chart2=document.querySelector('.chart2');
 const chart3=document.querySelector('.chart3');
-
-const makeChart =(percent,classname,color)=>{
+//참고 conic-gradient는 ie에서 지원하지 않음....
+const makeChart =(percent,classname,color,color2)=>{
     let i=1;
     let chartFn=setInterval(function(){
         if(i <=percent)
         {
-            colorFn(i,classname,color);
+            colorFn(i,classname,color,color2);
             i++
         }else{
             clearInterval(chartFn);
         }
     },10);
 }
-const colorFn = (i, classname, color) => {
-    classname.style.background = "conic-gradient(" + color + " 0% " + i + "%, #dedede " + i + "% 100%)";
+const colorFn = (i, classname, color,color2) => {
+    classname.style.background = "conic-gradient("+color +" " + i + "%,"+color2+" "+ i + "% 100%)";
   }
   
  
 
-makeChart(80, chart1, '#f5b914');
+makeChart(80, chart1, '#f5b914','red');
 makeChart(50, chart2, '#f5b914');
 makeChart(30, chart3, '#f5b914');
